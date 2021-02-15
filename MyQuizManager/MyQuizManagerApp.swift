14c50bea88404c1f56dec95836ce286c34002fda
YuliaPichugina <t-ypichugina@hotels.com>
2021-02-15 10:59:43 +0000

    Initial Commit

---
+++ MyQuizManager/MyQuizManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyQuizManagerApp.swift
+//  MyQuizManager
+//
+//  Created by Yulia Pichugina on 15/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyQuizManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
